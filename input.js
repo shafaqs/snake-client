57d94b51636a20b791560fccf97ef45f09d1596d
Shafaq Saud <shafaq.saud@gmail.com>
2022-10-24 14:41:42 -0400

    export module

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function() {
+  console.log("I'm here");
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+const handleUserInput = function(data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,26 +1,12 @@
 
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // establishes a connection with the game server
 
 console.log("Connecting ...");
 connect();
-const setupInput = function() {
-  console.log("I'm here");
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
 
-
-  return stdin;
-};
-const handleUserInput = function(data) {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
 
 setupInput();
 //stdin.on("data", handleUserInput());
